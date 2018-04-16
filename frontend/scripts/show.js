ec077f9ca548fca9353e4edd5ff8b2108cb4ecfc
tuzhiguang <tuzhiguang@star-net.cn>
2018-04-16 18:23:10 +0800

    DOC 测试vue

--- frontend/scripts/show.js
+++ frontend/scripts/show.js
@@ -2,7 +2,36 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'hello world!'
+    message: 'hello world!',
+    obj: {},
+    firstName: '',
+    lastName: '',
+    rawHtml: '<img src="show.js" onerror="alert(document.cookie)">'
+  },
+  methods: {
+    print() {
+      console.log('hello ,tutu')
+    }
+  },
+  watch: {
+    message(newvalue, oldvalue) {
+      alert('changed' + newvalue + oldvalue)
+    }
+  },
+  computed: {
+    reverse() {
+      return this.message.split('').reverse().join('')
+    },
+    fullName: {
+      get() {
+        return this.firstName + this.lastName
+      },
+      set(newValue) {
+        const tmp = newValue.split(' ')
+        this.firstName = tmp[0]
+        this.lastName = tmp[1]
+      }
+    }
   }
 })
 const name = 'tutu'
