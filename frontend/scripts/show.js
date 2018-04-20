a58e58aefb43e2897f90ed69b5c9bba40b8cbc41
tuzhiguang <tuzhiguang@star-net.cn>
2018-04-20 18:02:38 +0800

    DOC 测试vue lifecycle

--- frontend/scripts/show.js
+++ frontend/scripts/show.js
@@ -1,23 +1,43 @@
 'use strict'
 const app = new Vue({
-  el: '#app',
   data: {
     message: 'hello world!',
-    obj: {},
+    obj: {name: 'sss'},
     firstName: '',
     lastName: '',
     rawHtml: '<img src="show.js" onerror="alert(document.cookie)">'
+  },
+  beforeCreate() {
+    console.log('beforeCreated' + this.message)
+  },
+  created() {
+    console.log('created'  + this.message)
+  },
+  beforeMount() {
+    console.log('beforeMounted' + this.message)
+  },
+  mounted() {
+    const self = this
+    this.$nextTick(function () {
+      console.log('xxxx')
+      setTimeout(function () {
+        self.obj.name = 'hello'
+      }, 1000)
+    })
+  },
+  updated() {
+    console.log('updated' + this.message)
   },
   methods: {
     print() {
       console.log('hello ,tutu')
     }
   },
-  watch: {
-    message(newvalue, oldvalue) {
-      alert('changed' + newvalue + oldvalue)
-    }
-  },
+  // watch: {
+  //   message(newvalue, oldvalue) {
+  //     alert('changed' + newvalue + oldvalue)
+  //   }
+  // },
   computed: {
     reverse() {
       return this.message.split('').reverse().join('')
@@ -34,5 +54,5 @@ const app = new Vue({
     }
   }
 })
-const name = 'tutu'
+app.$mount('#app')
 
